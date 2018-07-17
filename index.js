8bb8aa001ee2a33db0eece7f40fcf7e063340108
Thor Denson <tjdenson@eckerd.edu>
2018-07-17 10:35:21 -0400

    adds indexes

---
+++ index.js
@@ -0,0 +1,13 @@
+
+//Vanilla DOM Programming 
+// let hello = document.createElement('h1');
+// hello.textContent = "Allo there";
+
+// // jQuery version
+// let jqHello = $('<h1>',{
+//     text: " Oi! Ello Mate"
+// });
+
+//React Version 
+React.createElement('h1', null, 'hello react!');
+ReactDOM.render 
