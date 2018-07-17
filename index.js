6ad837cb7416919d5e716b5813d0428500723a0a
Thor Denson <tjdenson@eckerd.edu>
2018-07-17 10:48:54 -0400

    adds children

--- index.js
+++ index.js
@@ -2,12 +2,19 @@
 //Vanilla DOM Programming 
 // let hello = document.createElement('h1');
 // hello.textContent = "Allo there";
+//document.querySelector('[data-root]').appendChild(hello!);
 
 // // jQuery version
 // let jqHello = $('<h1>',{
 //     text: " Oi! Ello Mate"
 // });
+//jqHello.appendTo('[data-root]');
 
 //React Version 
-React.createElement('h1', null, 'hello react!');
-ReactDOM.render 
+let hello = React.createElement('h1', null, 'hello react!');
+let item = React.createElement('li', null, 'chicken hearts');
+let list = React.createElement('ul', null, item);
+
+let main = React.createElement('div', null, 
+
+ReactDOM.render(hello, document.querySelecter('[data-root]')); 
